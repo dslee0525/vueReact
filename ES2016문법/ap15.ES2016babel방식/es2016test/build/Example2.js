51dd2cca259911100031749891bc906a86b0504a
ssss <sss@gmail.com>
2021-10-10 18:08:59 +0900

    day05_react

---
+++ ES2016문법/ap15.ES2016babel방식/es2016test/build/Example2.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var _Example = require('./Example');
+
+var _Example2 = _interopRequireDefault(_Example);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+//default는 파일당 한번만 사용가능
+// export 는 { } 안에 넣어줘야함
+console.log(_Example.a, _Example2.default, _Example.c);
---
+++ ES2016문법/ap15.ES2016babel방식/es2016test/src/Example2.js
@@ -0,0 +1,4 @@
+import b, {a, c} from './Example';
+//default는 파일당 한번만 사용가능
+// export 는 { } 안에 넣어줘야함
+console.log(a, b, c);
